21aed136d34df0d4e3bad4ea16aa628af36c055d
hulkSpirit <hulk-m@yandex.ru>
2017-01-02 09:46:26 +0200

    Создание контсруктора для управления данными

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -12,7 +12,18 @@ namespace Snake
         public int y;
         public char sym;
 
-        public void Drow()
+        public Point()
+        {
+        }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
